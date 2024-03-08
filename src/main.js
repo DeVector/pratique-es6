92aa29bd0b3aa74ce6b50f4bd14ec7f449d08180
DeVector <diniz.vito@gmail.com>
2024-03-08 17:40:17 -0300

    Commit initial

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const alunos = [
+    {nome: 'Victor', nota: 4},
+    {nome: 'Pedro', nota: 10},
+    {nome: 'João', nota: 6},
+    {nome: 'Antonio', nota: 7},
+    {nome: 'Andre', nota: 9},
+    {nome: 'Cacio', nota: 3},
+]
+
+const notasMaiorQueSeis = alunos.map( alunoAtual => {
+    if(alunoAtual.nota >= 6 ){
+        return alunoAtual.nota;
+    }
+}).filter(Boolean);//Utilizado para fazer com que as notas que não foram selecionadas não apareçam undifined.
+
+console.log(notasMaiorQueSeis);
